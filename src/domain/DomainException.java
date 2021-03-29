a868a41a9751d6e2892eca4b01de82454a9fd04c
TomSente <tom.sente@hotmail.com>
2021-03-29 15:01:03 +0200

    Class Speler: Daniel + Tom

---
+++ src/domain/DomainException.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class DomainException extends RuntimeException {
+    public DomainException()
+    {
+        super();
+    }
+
+    public DomainException(String message)
+    {
+        super(message);
+    }
+}
--- src/domain/Speler.java
+++ src/domain/Speler.java
@@ -1,4 +1,77 @@
 package domain;
 
 public class Speler {
+    private String naam;
+    private int score;
+
+    public Speler(String naam, int score)
+    {
+        setNaam(naam);
+        setScore(score);
+    }
+
+    public Speler(String naam)
+    {
+        setNaam(naam);
+        setScore(0);
+    }
+
+
+    public void setNaam(String naam) {
+        if(naam==null||naam.trim().isEmpty())
+        {
+            throw new DomainException("naam mag niet leeg zijn");
+        }
+        this.naam = naam;
+    }
+
+    public void setScore(int score)
+    {
+        if(score<0)
+        {
+            throw new DomainException("score mag niet negatief zijn");
+        }
+        this.score = score;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addToScore(int i )
+    {
+        if(this.score+i<0)
+        {
+            throw new DomainException("resulterende score mag niet negatief zijn");
+        }
+        this.score+=i;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        boolean result=false;
+        if(o instanceof Speler)
+        {
+            Speler s = (Speler) o;
+            if(this.getNaam().equals(s.getNaam()) && this.getScore()==(s.getScore()))
+            {
+                result=true;
+            }
+        }
+        return result;
+    }
+
+
+    public String toString()
+    {
+        return this.getNaam()+" heeft een score van "+this.getScore();
+    }
+
+
 }
+
